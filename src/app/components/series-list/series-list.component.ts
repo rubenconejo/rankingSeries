b6e38e31db8689d07791c09d0327841ff7ab0bcb
rubenconejo <ruben8723@gmail.com>
2024-10-29 16:26:59 +0100

    add series list component

--- src/app/components/series-list/series-list.component.ts
+++ src/app/components/series-list/series-list.component.ts
@@ -11,14 +11,50 @@ import { SeriesService } from '../../services/series.service';
 })
 export class SeriesListComponent {
 
-  series:any [] = [];
+  series: any[] = [];
 
-  constructor(private seriesService: SeriesService){}
+  constructor(private seriesService: SeriesService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.seriesService.getSeries().subscribe((data) => {
       this.series = data;
       console.log(this.series);
+      this.series = this.groupAndSortSeries(data);
     })
+  }
+
+  private groupAndSortSeries(seriesList: any[]): any[] {
+    const groupedSeries = this.groupSeries(seriesList); // Agrupamos
+    const seriesWithAverage = this.calculateAverageRating(groupedSeries); // Calculamos la media
+
+    // Ordenamos alfabéticamente y retornamos
+    return seriesWithAverage.sort((a: any, b: any) => a.nombre.localeCompare(b.nombre));
+  }
+
+
+  private groupSeries(seriesList: any[]): any[] {
+    const grouped = seriesList.reduce((acc, serie) => {
+      if (!acc[serie.nombre]) {
+        acc[serie.nombre] = {
+          nombre: serie.nombre,
+          plataforma: serie.plataforma,
+          valoraciones: []
+        };
+      }
+      acc[serie.nombre].valoraciones.push(...serie.valoraciones);
+      return acc;
+    }, {});
+
+    // Convertimos el objeto agrupado a un array para facilitar el cálculo de la media
+    return Object.values(grouped);
+  }
+
+  private calculateAverageRating(seriesList: any[]): any[] {
+    return seriesList.map((serie) => {
+      const totalScore = serie.valoraciones.reduce((sum: number, valoracion: any) => sum + valoracion.puntuacion, 0);
+      const quantityReviews = serie.valoraciones.length;
+      serie.valoracionMedia = quantityReviews ? (totalScore / quantityReviews) : 0;
+      return serie;
+    });
   }
 }
