84066514d2feae71bd48a58b2bc511776028ccd3
rubenconejo <ruben8723@gmail.com>
2024-11-01 17:18:45 +0100

    add router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { SeriesListComponent } from './components/series-list/series-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SeriesListComponent, NavbarComponent],
+  imports: [RouterModule, SeriesListComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
