b64e6a067a6c96a7c7b6fdfd30cc678b083073bc
rubenconejo <ruben8723@gmail.com>
2024-11-01 17:19:02 +0100

    create paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { SeriesListComponent } from './components/series-list/series-list.component';
+import { AddSerieComponent } from './components/add-serie/add-serie.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'series', component: SeriesListComponent }, // Listado de series
+    { path: 'add-serie', component: AddSerieComponent },
+    { path: '', pathMatch: 'full', redirectTo:'series'}, // Añadir nueva 
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
